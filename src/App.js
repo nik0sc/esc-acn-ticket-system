ec407be3ce0eb55ffec2074d731da10e4ea7b3ad
Kim <kohseukim@Kohs-MacBook-Pro.local>
2019-02-23 00:12:10 +0800

    login/register page

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React, { Component } from 'react';
-import Form from './Components/Form'
-import axios from 'axios'
+// import React, { Component } from 'react';
+// import Form from './Components/Form'
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import Register from './Components/Register'
 
+
+// class App extends Component {
+  
+//   render() {
+//     return (
+//       <Router>
+//       <div className="App">
+//       <Switch>
+//       <Route exact path="/" Component={Form} ></Route>
+//       <Route path="/signin" Component={Register}></Route>
+//       </Switch>
+//       </div>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Register from './Components/Register'
+import Form from './Components/Form';
+
+import './App.css';
 
 class App extends Component {
-  
-  getUser = async(e) => {
-    e.preventDefault();
-    const username = e.target.elements.username.value;
-    const password = e.target.elements.password.value;
-    if(username && password){
-      axios.get(`https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/login?username=${username}&password=${password}`, {
-        headers: {
-          'Server-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJnWVppRDZzbzJLcXNMT1hmVUt5TjZpdHVXUXhaQnkyN0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI2NDksImV4cCI6MTU1MjU0NDY0OSwiYXpwIjoiZ1laaUQ2c28yS3FzTE9YZlVLeU42aXR1V1F4WkJ5MjciLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.uSISsVSh1REzY3UuMWm_QTEd4xs10cqoWtQpHj3xz9HhKx1_N0s4Wj7A-rQRsQJzQ12IiB5A05lQ17DdkaQkfi_4zeNTGQTo3MvE9Glf1wfcWCMe2WAPr78GSL0RQKuyKZpwrlFuxNghN_-sEVrG4gI7VZyWEc6S_m2076TXVPigTF29u9dA6NgzQkVRaqssulgO_SaZtG9mFwAJ19CaQluqrx10GHsd6OKN2YXPzvSBFa2ouUHlncePbgtKsOl660MQFnyTGtLTzYZPJRX7mpTHSSb4RWoY45lwtt5vfV0HwSC84nKyZvfkK6frFZkpltfSjiWRo6R62lzt5r1dcw'
-        }
-      })
-      .then((res) => {
-        console.log(res)
-      }
-      )
-    }
-  }
   render() {
     return (
-      <div className="App">
-        <div><Form getUser={this.getUser}/></div>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="App__Aside"></div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+              </div>
+
+              <div className="FormTitle">
+                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+              </div>
+
+              <Route exact path="/" component={Register}>
+              </Route>
+              <Route path="/sign-in" component={Form}  >
+              </Route>
+          </div>
+
+        </div>
+      </Router>
     );
   }
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,16 +1,102 @@
 import React from 'react'
+import axios from 'axios'
+
 
 class Form extends React.Component{
+
+    getUser = async(e) => {
+        e.preventDefault();
+        const username = e.target.elements.username.value;
+        const password = e.target.elements.password.value;
+        if(username && password){
+          axios.get(`https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/login?username=${username}&password=${password}`, {
+            headers: {
+              'Server-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJnWVppRDZzbzJLcXNMT1hmVUt5TjZpdHVXUXhaQnkyN0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI2NDksImV4cCI6MTU1MjU0NDY0OSwiYXpwIjoiZ1laaUQ2c28yS3FzTE9YZlVLeU42aXR1V1F4WkJ5MjciLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.uSISsVSh1REzY3UuMWm_QTEd4xs10cqoWtQpHj3xz9HhKx1_N0s4Wj7A-rQRsQJzQ12IiB5A05lQ17DdkaQkfi_4zeNTGQTo3MvE9Glf1wfcWCMe2WAPr78GSL0RQKuyKZpwrlFuxNghN_-sEVrG4gI7VZyWEc6S_m2076TXVPigTF29u9dA6NgzQkVRaqssulgO_SaZtG9mFwAJ19CaQluqrx10GHsd6OKN2YXPzvSBFa2ouUHlncePbgtKsOl660MQFnyTGtLTzYZPJRX7mpTHSSb4RWoY45lwtt5vfV0HwSC84nKyZvfkK6frFZkpltfSjiWRo6R62lzt5r1dcw'
+            }
+          })
+          .then((res) => {
+            console.log(res)
+          }
+          )
+        }
+      }
+      
     render(){
         return(
-            <form onSubmit={this.props.getUser}>
+            <form onSubmit={this.getUser}>
                 <input type="text" name="username" placeholder="Username" />
                 <input type="text" name="password" placeholder="Password" />
                 <button> Login </button>
-                <button> Register </button>
             </form>
         )
     }
 }
 
 export default Form;
+
+
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+// class Form extends Component {
+//     constructor() {
+//         super();
+
+//         this.state = {
+//             email: '',
+//             password: ''
+//         };
+
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     handleChange(e) {
+//         let target = e.target;
+//         let value = target.type === 'checkbox' ? target.checked : target.value;
+//         let name = target.name;
+
+//         const username = e.target.elements.name.value;
+//         const password = e.target.elements.password.value;
+//         if(username && password){
+
+//             this.setState({
+//                 email: this.state.username,
+//                 password: this.state.password
+//               });
+
+//         }
+
+    
+//     }
+
+//     handleSubmit(e) {
+//         e.preventDefault();
+
+//         console.log('The form was submitted with the following data:');
+//         console.log(this.state);
+//     }
+
+//     render() {
+//         return (
+//         <div>
+//             <form onSubmit={this.handleSubmit}>
+//             <div>
+//                 <label>Name</label>
+//                 <input type="email" id="name" placeholder="Enter your email" name="name" value={this.state.name} onChange={this.handleChange} />
+//               </div>
+//               <div>
+//                 <label>Password</label>
+//                 <input type="password" id="password" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+//               </div>
+
+//               <div>
+//                   <button>Sign In</button> <Link to="/">Create an account</Link>
+//               </div>
+//             </form>
+//           </div>
+//         );
+//     }
+// }
+
+// export default Form;
