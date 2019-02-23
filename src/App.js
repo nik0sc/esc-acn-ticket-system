5f568297aa884e81aea7dbd9052d7150f34a1d1b
Kim <kohseukim@Kohs-MacBook-Pro.local>
2019-02-23 14:58:08 +0800

    added some css

--- src/App.js
+++ src/App.js
@@ -1,57 +1,59 @@
-// import React, { Component } from 'react';
-// import Form from './Components/Form'
-// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-// import Register from './Components/Register'
-
-
-// class App extends Component {
-  
-//   render() {
-//     return (
-//       <Router>
-//       <div className="App">
-//       <Switch>
-//       <Route exact path="/" Component={Form} ></Route>
-//       <Route path="/signin" Component={Register}></Route>
-//       </Switch>
-//       </div>
-//       </Router>
-//     );
-//   }
-// }
-
-// export default App;
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Register from './Components/Register'
 import Form from './Components/Form';
+import Admin from './Components/Admin';
 
 import './App.css';
+import Dashboard from './Components/Dashboard';
 
 class App extends Component {
+  
   render() {
     return (
-      <Router>
-        <div className="App">
+      <div className="wrapper">
+        <div className="main"> 
+        <div className="container"> 
+          <div className="row" >
+          
+          <div className="col-sm-4 title-container">
+            <h1>hello</h1> 
+          </div>
+          <div className="col-sm-8 form-container">
+          <Router>
+
+            
+          <div className="App">
           <div className="App__Aside"></div>
           <div className="App__Form">
             <div className="PageSwitcher">
                 <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+                <NavLink exact to="/register" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
               </div>
 
-              <div className="FormTitle">
+              {/* <div className="FormTitle">
                   <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
-              </div>
+              </div> */}
 
-              <Route exact path="/" component={Register}>
+              <Route path="/register" component={Register}>
               </Route>
               <Route path="/sign-in" component={Form}  >
               </Route>
+              <Route path="/admin" component={Admin} >
+              </Route>
+          
           </div>
 
         </div>
-      </Router>
+        </Router>
+
+      </div>
+          </div>
+
+      </div>
+        </div>       
+      </div>
+
     );
   }
 }
---
+++ src/Components/Admin.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import axios from 'axios'
+
+
+class Admin extends React.Component{
+
+    getAdmin = async(e) => {
+        e.preventDefault();
+        const email = e.target.elements.email.value;
+        const password = e.target.elements.password.value;
+        if(email && password){
+          axios.get(`https://ug-api.acnapiv3.io/swivel/acnapi-common-services/common/login?email=${email}&password=${password}`, {
+            headers: {
+              'Server-Token':'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlF6Y3hRVEl5UkRVeU1qYzNSakEzTnpKQ01qVTROVVJFUlVZelF6VTRPRUV6T0RreE1UVTVPQSJ9.eyJpc3MiOiJodHRwczovL2FjbmFwaS1wcm9kLmF1dGgwLmNvbS8iLCJzdWIiOiJnWVppRDZzbzJLcXNMT1hmVUt5TjZpdHVXUXhaQnkyN0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9wbGFjZWhvbGRlci5jb20vcGxhY2UiLCJpYXQiOjE1NDk5NTI2NDksImV4cCI6MTU1MjU0NDY0OSwiYXpwIjoiZ1laaUQ2c28yS3FzTE9YZlVLeU42aXR1V1F4WkJ5MjciLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.uSISsVSh1REzY3UuMWm_QTEd4xs10cqoWtQpHj3xz9HhKx1_N0s4Wj7A-rQRsQJzQ12IiB5A05lQ17DdkaQkfi_4zeNTGQTo3MvE9Glf1wfcWCMe2WAPr78GSL0RQKuyKZpwrlFuxNghN_-sEVrG4gI7VZyWEc6S_m2076TXVPigTF29u9dA6NgzQkVRaqssulgO_SaZtG9mFwAJ19CaQluqrx10GHsd6OKN2YXPzvSBFa2ouUHlncePbgtKsOl660MQFnyTGtLTzYZPJRX7mpTHSSb4RWoY45lwtt5vfV0HwSC84nKyZvfkK6frFZkpltfSjiWRo6R62lzt5r1dcw'
+            }
+          })
+          .then((res) => {
+            console.log(res)
+          }
+          )
+        }
+      }
+      
+    render(){
+        return(
+            <form onSubmit={this.getAdmin}>
+                <input type="text" name="email" placeholder="Email" />
+                <input type="text" name="password" placeholder="Password" />
+                <button> Login </button>
+            </form>
+        )
+    }
+}
+
+export default Admin;
